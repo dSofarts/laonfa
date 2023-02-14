f3fbb0603afaea1c14dfb0d0ce0fc40d0c2d5d08
”Egor <dronovegorvl@yandex.com>
2023-02-14 23:27:10 +0300

    Initial commit

---
+++ src/main/java/com/dsofarts/laonfa/LaonfaApplication.java
@@ -0,0 +1,13 @@
+package com.dsofarts.laonfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaonfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaonfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsofarts/laonfa/LaonfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsofarts.laonfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaonfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
